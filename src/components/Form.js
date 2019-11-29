b47acbef60614df8ae00fc17535df3649ca3fd6e
El khaldi <dev.khaldi@gmail.com>
2019-11-29 23:38:35 +0100

    Form compo

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+            <form>
+                    <input type="text" placeholder="Find recipes" />
+                    <button>Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
