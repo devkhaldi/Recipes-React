a581d35357e1d4f44bd44e7cfab178b3d1957d1c
El khaldi <dev.khaldi@gmail.com>
2019-12-02 23:40:53 +0100

    Details compo

--- src/components/recipes/Details.js
+++ src/components/recipes/Details.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
+
+const API_KEY = "f3792d37f5b64c61bb4e585c489aac25"
+
 export class Details extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             recipe : {} 
+        }
+    }
+    
     render() {
         return (
-            <div>
-                <h1>Details compo</h1>
-            </div>
-        )
-    }
-}
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Recipe Details</h1>
+                    </header>
+                
+                
+
+                </div>
+            )
+        }
+ }
 
 export default Details
